54a1e4571cc06a5b79d59013ce5239d74d02784d
mainoo-smith <49887162+mainoo-smith@users.noreply.github.com>
2026-08-29 20:41:35 -0600

    Update funnel-config.js
    updates the funnel url

--- js/funnel-config.js
+++ js/funnel-config.js
@@ -1,6 +1,6 @@
 /** Public funnel config for the assessment forms. Ops: Notion runbook. */
 window.KONTROLIQ_FUNNEL = {
-  formEndpoint: "",
+  formEndpoint: "https://script.google.com/macros/s/AKfycbzc9XGE6R75tzNom1h_njfG4cN1cx2MBBlvgU3qjLFT1O1O42WZ6zkdoxzGxQPI7mq7/exec",
   calendlyUrl: "https://calendly.com/mainootechnologies",
   siteHome: "../index.html"
 };
